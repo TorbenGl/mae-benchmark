db7bd0e4340db703c12038b4073808620447833e
Torben Globisch <torben.globisch@uni-rostock.de>
2026-03-30 11:29:25 +0200

    updated wokrker

--- demo/submit_demo.sh
+++ demo/submit_demo.sh
@@ -6,14 +6,14 @@
 #SBATCH --cpus-per-task=4       # 4 CPUs per task
 #SBATCH --mem=2G
 #SBATCH --time=00:05:00
-#SBATCH --array=0-5%4           # 6 jobs, max 4 running in parallel (2 queue)
+#SBATCH --array=2          # 6 jobs, max 4 running in parallel (2 queue)
 #SBATCH --output=demo/logs/task_%A_%a.out
 #SBATCH --error=demo/logs/task_%A_%a.err
 
 echo "=== SLURM task ${SLURM_ARRAY_TASK_ID} / job ${SLURM_JOB_ID} ==="
 echo "Running on: $(hostname)"
 echo "Started at: $(date)"
-
+which python
 python demo/worker.py
 
 echo "Finished at: $(date)"
